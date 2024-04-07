e9d2e0b33fee6cd9fc6c13662f0312cebed21d75
dehyabi <dehyasvibe@gmail.com>
2024-04-08 05:54:41 +0700

    feature(laporan): add view to display laporan

--- resources/views/laporan/menu-laporan.blade.php
+++ resources/views/laporan/menu-laporan.blade.php
@@ -1,1 +1,69 @@
-laporan
+@extends('layouts.main')
+@section('content')
+<div class="card mb-4">
+    <div class="card-header">
+        <div class="row">
+            <div class="col">
+                Laporan > Laporan Petty Cash
+            </div>
+            <div class="col text-right">
+                <a href="" ><button class="btn btn-info">Filter</button></a>
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+        @if(Session::has('success'))
+            <div class="alert alert-success mb-3 text-center" role="alert">{{ Session::get('success') }}</div>
+        @endif
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Tanggal</th>
+                        <th>Jenis Pembelian</th>
+                        <th>Penerimaan</th>
+                        <th>Pengeluaran</th>
+                        <th>Akun Petty Cash</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
+@section('script')
+<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        
+<script>
+   $('#dataTable').DataTable({
+    columnDefs: [
+    {bSortable: false, targets: [4]} 
+  ],
+                dom: 'lBfrtip',
+           buttons: [
+               {
+                   extend: 'excelHtml5',
+                   exportOptions: {
+                    modifier: {
+                        page: 'current'
+                    },
+                    columns: [ 0, 1, 2, 3, 4]
+                   }
+               },
+               {
+                   extend: 'pdfHtml5',
+                   exportOptions: {
+                    modifier: {
+                        page: 'current'
+                    },
+                    columns: [ 0, 1, 2, 3, 4 ]
+                   }
+               }
+           ]
+           });
+       </script>
+@endsection
