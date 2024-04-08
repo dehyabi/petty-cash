1c0468f8b916da9cf310b2a857c451519687ae52
dehyabi <dehyasvibe@gmail.com>
2024-04-08 07:40:29 +0700

    feature(dashboard): add view for dashboard

--- resources/views/dashboard/menu-dashboard.blade.php
+++ resources/views/dashboard/menu-dashboard.blade.php
@@ -1,1 +1,101 @@
-dashboard
+@extends('layouts.main')
+@section('content')
+
+<div class="card-header">
+        <div class="row">
+            <div class="col">
+                Dashboard > Petty Cash
+            </div>
+        </div>
+</div>
+
+<div class="row my-3 text-center">
+    <div class="col-md">
+        <a class="card-link" href="{{ route('menu.lokasi') }}">
+        <div class="card-db card-left card py-5 my-2">
+            Lokasi Outlet
+        </div>
+        </a>
+    </div>
+    <div class="col-md">
+        <a class="card-link" href="{{ route('menu.transaksi') }}">
+        <div class="card-db card-middle card py-5 my-2">
+            Transaksi
+        </div>
+        </a>
+    </div>
+    <div class="col-md">
+        <a class="card-link" href="{{ route('menu.laporan') }}">
+        <div class="card-db card-right card py-5 my-2">
+            Laporan
+        </div>
+        </a>
+    </div>
+</div>
+
+<div class="card mb-4">
+    <div class="card-header">
+        <div class="row">
+            <div class="col text-center">
+                Laporan Petty Cash Periode April 2024
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+        @if(Session::has('success'))
+            <div class="alert alert-success mb-3 text-center" role="alert">{{ Session::get('success') }}</div>
+        @endif
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Tanggal</th>
+                        <th>Toko</th>
+                        <th>Akun</th>
+                        <th>User</th>
+                        <th>Jenis Pembelian</th>
+                        <th>Penerimaan</th>
+                        <th>Pengeluaran</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
+@section('script')
+<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        
+<script>
+   $('#dataTable').DataTable({
+    columnDefs: [
+    {bSortable: false, targets: [4]} 
+  ],
+                dom: 'lBfrtip',
+           buttons: [
+               {
+                   extend: 'excelHtml5',
+                   exportOptions: {
+                    modifier: {
+                        page: 'current'
+                    },
+                    columns: [ 0, 1, 2, 3, 4]
+                   }
+               },
+               {
+                   extend: 'pdfHtml5',
+                   exportOptions: {
+                    modifier: {
+                        page: 'current'
+                    },
+                    columns: [ 0, 1, 2, 3, 4 ]
+                   }
+               }
+           ]
+           });
+       </script>
+@endsection
